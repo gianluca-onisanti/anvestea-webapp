0bd71d416c0eec7aa669c71b618589e481985366
Gianluca Onisanti <gianluca.onisanti@gmail.com>
2024-10-08 16:55:02 -0300

    Criação de Menu Lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -6,9 +6,25 @@ import SideMenu from './components/SideMenu';
 import { Box } from '@mui/material';
 
 const App: React.FC = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e : any) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);   
+
+
+    // Remove o event listener quando o componente for desmontado
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
 
   return (
     <BrowserRouter>
+    <div className='light'
+      style={{ left: `${position.x}px`, top: `${position.y}px` }}>
+    </div>
       <Box sx={{ display: 'flex' }}>
         <SideMenu />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -10,9 +10,9 @@ import InfoIcon from '@mui/icons-material/Info';
 import { Link, NavLink } from 'react-router-dom';
 import ListItemDivider from './ListIemDivider';
 import { Box, Typography } from '@mui/material';
+import pointer from '../styles/pointer.png'
 
-
-const drawerWidth = 265;
+const drawerWidth = 265
 
 const listItem = [
     { 
@@ -27,65 +27,73 @@ const listItem = [
     }
 ]
 
-const SideMenu: React.FC = () => (
-    <Drawer
-        sx={{
-            mr: '64px',
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-                backgroundColor: '#f1e7e7',
-                width: drawerWidth,
-                boxSizing: 'border-box',
-            },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={true}
-    >
-        <Box 
-            height='12px' 
-            width='auto' 
-            padding={2}
-            mb={4}
-            textAlign='center'
+const SideMenu: React.FC = () => {
+
+    const [clicked, setClicked] = React.useState(0)
+
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth + 64,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    backgroundColor: '#f1e7e7',
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                }
+            }}
+            variant="persistent"
+            anchor="left"
+            open={true}
         >
-            <Typography borderBottom={1} fontFamily='anvestea-bold' fontSize={38}>
-                Anvestea's Accord
-            </Typography>
-        </Box>
-        <List>
+            <Box
+                height='12px'
+                padding={2}
+                mb={4}
+                textAlign='center'
+            >
+                <Typography
+                    borderBottom={1}
+                    fontFamily='anvestea-bold'
+                    fontSize={38}
+                    color='#1a0e0e'
+                >
+                    Anvestea's Accord
+                </Typography>
+            </Box>
+            <List>
                 {listItem.map((item: any, key: number) => (
                     <>
-                    <ListItemDivider />
-                    <ListItem 
-                        key={key}
-                        disablePadding 
-                        component={NavLink} 
-                        to={item.route} 
-                        sx={{
-                            color: '#1a0e0e',
-                            ml: '10px',
-                            '&.active': {
-                                backgroundColor: 'lightgray'
-                            }
-                    }}>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {item.icon}
-                            </ListItemIcon>
-                            <ListItemText sx={{ height: 'auto', ml: '-10px'}} primary={(
-                                <Typography color='black' fontFamily='fredoka'>
-                                    {item.label}
-                                </Typography>
-                            )} />
-                        </ListItemButton>
-                    </ListItem>
+                        <ListItemDivider />
+                        <ListItem
+                            key={key}
+                            disablePadding
+                            component={NavLink}
+                            onClick={() => setClicked(key)}
+                            to={item.route}
+                            sx={{
+                                color: clicked === key ? '#a3595c' : '#1a0e0e',
+                                backgroundColor: clicked === key ? '#1a0e0e' : null
+                            }}>
+                            <ListItemButton sx={{'&:hover': { cursor: `url(${pointer}), pointer` }}}>
+                                <ListItemIcon>
+                                    <div style={{ marginTop: '5px', color: clicked === key ? '#a3595c' : '#1a0e0e' }}>
+                                        {item.icon}
+                                    </div>
+                                </ListItemIcon>
+                                <ListItemText sx={{ height: 'auto', ml: '-10px' }} primary={(
+                                    <Typography fontFamily='fredoka'>
+                                        {item.label}
+                                    </Typography>
+                                )} />
+                            </ListItemButton>
+                        </ListItem>
                     </>
                 ))}
                 <ListItemDivider />
-        </List>
-    </Drawer>
-);
+            </List>
+        </Drawer>
+    )
+}
 
 export default SideMenu;
